51aef285e0145dea261269dd181399ff0995a902
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-02-14 11:03:20 +1300

    created appReducer nad btn what creates todos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 // import logo from './logo.svg'
 import './App.css'
 
+function appReducer(state, action) {
+  switch (action.type) {
+    case 'add': {
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          text: '',
+          completed: false,
+        }
+      ]
+    }
+    default:
+      break;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(appReducer, [])
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Todos App</h1>
+      <button onClick={() => dispatch({ type: 'add'})}>
+        New Todo
+      </button>
+      {state.map(item => (
+        <div key={item.id}>{item.id}</div>
+      ))}
     </div>
   )
 }
